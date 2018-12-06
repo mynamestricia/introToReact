6cc65e47ebbada2a6ff650774edc1fc9edda71e5
Tricia Albert <tricia.albert@gmail.com>
2018-12-06 15:19:20 -0500

    First commit React ToDoApp

---
+++ ToDoApp/react-to-do/src/components/ToDo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ToDo extends Component {
+  render() {
+    return (
+      <div>
+        <li> Make bed </li>
+        <li> Do dishes </li>
+      </div>
+    );
+  }
+}
+export default ToDo;
